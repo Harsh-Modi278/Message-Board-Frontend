e5f1105971aad92011e977875c1691ee3548fe2c
Harsh-Modi278 <harshmodi278@gmail.com>
2021-10-12 17:07:41 +0530

    feat: made footer sticky

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,8 +18,19 @@ function Copyright() {
 
 function Footer(props) {
   return (
-    <Box component="footer" sx={{ bgcolor: "background.paper", py: 6 }}>
-      <Container maxWidth="lg">
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container maxWidth="sm">
         <Copyright />
       </Container>
     </Box>
