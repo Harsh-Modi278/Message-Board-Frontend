7e2dd68ac3d40f665d631c312d0103d227acf0be
Harsh-Modi278 <harshmodi278@gmail.com>
2021-10-13 16:56:36 +0530

    fix: remove redundant statements and imports

--- src/components/FeaturedBoards.js
+++ src/components/FeaturedBoards.js
@@ -1,22 +1,10 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
 import FeaturedBoard from "./FeaturedBoard";
 import useFetch from "../hooks/useFetch";
 
-const useStyles = makeStyles((theme) => ({
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-  link: {
-    textDecoration: "none",
-  },
-}));
-
 const FeaturedBoards = (props) => {
-  const classes = useStyles();
   const { sort } = props;
 
   //GET all articles for Home page
@@ -25,8 +13,6 @@ const FeaturedBoards = (props) => {
     isPending,
     error,
   } = useFetch(`http://localhost:5000/api/boards/` + (sort && `?sort=${sort}`));
-
-  console.log(`http://localhost:5000/api/boards/` + (sort && `?sort=${sort}`));
 
   return (
     <React.Fragment>
