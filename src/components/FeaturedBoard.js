7efc669a4f6db22b7b50105582050109f2604ff2
Harsh-Modi278 <harshmodi278@gmail.com>
2021-10-15 11:00:12 +0530

    fix: use ReactMarkdownWrapper to display board description

--- src/components/FeaturedBoard.js
+++ src/components/FeaturedBoard.js
@@ -2,11 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Link } from "react-router-dom";
 import routes from "../constants/route.json";
+import ReactMarkdownWrapper from "../components/ReactMarkdownWrapper";
+
 import { getTimeDiff } from "../utils/functions";
 
 const useStyles = makeStyles({
@@ -26,43 +27,44 @@ const FeaturedBoard = (props) => {
   const { post } = props;
 
   return (
-    <Grid item xs={12} md={6}>
-      <Link
-        to={`${routes.BOARD}/${post.board_id}`}
-        style={{ textDecoration: "none" }}
-      >
-        <Card className={classes.card}>
-          <div className={classes.cardDetails}>
-            <CardContent>
-              <Typography component="div" variant="h4" color="primary">
-                {post.board_name}
-              </Typography>
-              <Typography
-                variant="bottom text"
-                component="span"
-                color="secondary"
-              >
-                {`${post.upvotes} upvote ${post.upvotes > 1 ? "s" : ""} | ${
-                  post.comments_count
-                } comment${
-                  post.comments_count > 1 ? "s" : ""
-                } | submitted ${getTimeDiff(post.time_created)}`}
-              </Typography>
-              <br />
-              <br />
-              <Typography variant="subtitle1" paragraph component="p">
-                {post.preview &&
-                  post.preview.slice(0, 100) +
-                    (post.preview.length > 100 ? `...` : "")}
-              </Typography>
-              <Typography variant="subtitle1" color="primary" component="em">
-                Continue reading...
-              </Typography>
-            </CardContent>
-          </div>
-        </Card>
-      </Link>
-    </Grid>
+    <Link
+      to={`${routes.BOARD}/${post.board_id}`}
+      style={{ textDecoration: "none" }}
+    >
+      <Card className={classes.card}>
+        <div className={classes.cardDetails}>
+          <CardContent>
+            <Typography component="div" variant="h4" color="primary">
+              {post.board_name}
+            </Typography>
+            <Typography
+              variant="bottom text"
+              component="span"
+              color="secondary"
+            >
+              {`${post.upvotes} upvote ${post.upvotes > 1 ? "s" : ""} | ${
+                post.comments_count
+              } comment${
+                post.comments_count > 1 ? "s" : ""
+              } | submitted ${getTimeDiff(post.time_created)}`}
+            </Typography>
+            <br />
+            <Typography variant="subtitle1" paragraph component="p">
+              <ReactMarkdownWrapper
+                body={
+                  post.preview &&
+                  post.preview.slice(0, 400) +
+                    (post.preview.length > 400 ? `...` : "")
+                }
+              />
+            </Typography>
+            <Typography variant="subtitle1" color="primary" component="em">
+              Continue reading...
+            </Typography>
+          </CardContent>
+        </div>
+      </Card>
+    </Link>
   );
 };
 
