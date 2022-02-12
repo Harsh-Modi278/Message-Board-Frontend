8e0ce574613320f7ed5f433ae800b128002fb833
Harsh-Modi278 <harshmodi278@gmail.com>
2022-02-12 17:00:42 +0530

    feat: add pagination on home page

--- src/components/FeaturedBoards.js
+++ src/components/FeaturedBoards.js
@@ -1,12 +1,20 @@
-import { React } from "react";
+import { React, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Stack from "@mui/material/Stack";
 import FeaturedBoard from "./FeaturedBoard";
 import useFetch from "../hooks/useFetch";
-import {prefURL} from "../constants/backendURL"
+import { prefURL } from "../constants/backendURL";
+import {
+  PageNavigation,
+  Pagination as CustomPagination,
+} from "../utils/Pagination";
+import Pagination from "@mui/material/Pagination";
+import Typography from "@mui/material/Typography";
 
 const FeaturedBoards = (props) => {
   const { sort } = props;
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   //GET all articles for Home page
   let {
@@ -14,6 +22,7 @@ const FeaturedBoards = (props) => {
     isPending,
     error,
   } = useFetch(`${prefURL}/api/boards/` + (sort && `?sort=${sort}`));
+  const filteredPages = new CustomPagination(featuredBoards, 5);
 
   return (
     <>
@@ -22,10 +31,26 @@ const FeaturedBoards = (props) => {
       {isPending && <div>Loading...</div>}
       <main style={{ paddingTop: "1rem" }}>
         <Stack spacing={2} justifyContent="space-evenly">
-          {featuredBoards &&
-            featuredBoards.map((post) => (
-              <FeaturedBoard key={post.board_id} post={post} />
-            ))}
+          {filteredPages &&
+            filteredPages
+              .getPage(currentPage)
+              ?.map((post) => (
+                <FeaturedBoard key={post.board_id} post={post} />
+              ))}
+        </Stack>
+        <Stack spacing={1}>
+          <Typography ml={1} mt={1}>
+            Page: {currentPage}
+          </Typography>
+          <Pagination
+            count={filteredPages.getTotalPages()}
+            page={currentPage}
+            color="primary"
+            size="large"
+            onChange={(event, value) => {
+              setCurrentPage(value);
+            }}
+          />
         </Stack>
       </main>
     </>
---
+++ src/utils/Pagination.js
@@ -0,0 +1,36 @@
+export class Pagination {
+  constructor(data, pageSize) {
+    this.data = data;
+    this.pageSize = pageSize;
+  }
+
+  getPage(n) {
+    const offset = (n - 1) * this.pageSize;
+    return this.data?.slice(offset, offset + this.pageSize);
+  }
+
+  getTotalPages() {
+    return Math.floor(this?.data?.length / this?.pageSize) + 1;
+  }
+}
+
+export const PageNavigation = ({
+  nextPageHandler,
+  previousPageHandler,
+  currentPage,
+  totalPages,
+}) => {
+  return (
+    <div>
+      {currentPage === 0 ? null : (
+        <button onClick={previousPageHandler}>Prev</button>
+      )}
+      {currentPage + 1 >= totalPages ? null : (
+        <button onClick={nextPageHandler}>Next</button>
+      )}
+      <span>
+        Page {currentPage + 1} of {totalPages}
+      </span>
+    </div>
+  );
+};
